685b241cd90ef070bbc1cd3e13ebb8c08ffbf003
MrDNAlex <93613553+MrDNAlex@users.noreply.github.com>
2024-04-15 20:59:29 -0400

    Quick Fix for the IR Spectrum Graph

--- OrcaAnalysis/Scripts/IRSpectrum.py
+++ OrcaAnalysis/Scripts/IRSpectrum.py
@@ -48,9 +48,40 @@ intensities = intensities / np.max(intensities) * 100
 # Reverse the Intensities
 reverseIntensities = 100 - intensities
 
+
+sorted_pairs = sorted(zip(frequencies, reverseIntensities))
+
+# Unpacking the sorted pairs
+sorted_freq, sorted_intensity = zip(*sorted_pairs)
+
+# Convert tuples back to lists (if necessary)
+sorted_freq = list(sorted_freq)
+sorted_intensity = list(sorted_intensity)
+
+#Round the Values for less Decimal points
+sorted_freq = [round(num, 2) for num in sorted_freq]
+sorted_intensity = [round(num, 2) for num in sorted_intensity]
+
+#Find the Peaks (Mins in this case)
+peaks = []
+for i in range(1, len(sorted_intensity)-1):
+    if sorted_intensity[i-1] > sorted_intensity[i] < sorted_intensity[i+1] and sorted_intensity[i] < 90:
+        peaks.append(i)
+
+
 #Plot the Graph and Show
-plt.plot(frequencies, reverseIntensities, 'o')
+plt.plot(sorted_freq, sorted_intensity)
 plt.xlabel('Frequency')
 plt.ylabel('Intensity')
 plt.title(fileName.split(".")[0] + "  (" + fullPath + ")")
+
+# Annotate peaks
+for peak in peaks:
+    plt.annotate(f'({sorted_freq[peak]}, {sorted_intensity[peak]})', # text to display
+                 (sorted_freq[peak], sorted_intensity[peak]),        # point to annotate
+                 textcoords="offset points", # how to position the text
+                 xytext=(45,0),             # distance from text to points (x,y)
+                 ha='center')               # horizontal alignment can
+
+
 plt.show()
